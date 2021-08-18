5f45c42a0cdfb529f182a6aaa4c88acf9e3e1205
jjwon915 <jjwon915@naver.com>
2021-08-18 21:49:58 +0900

    settings.

---
+++ prac-spring/prac-spring/src/main/java/prac/pracspring/PracSpringApplication.java
@@ -0,0 +1,14 @@
+package prac.pracspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PracSpringApplication.class, args);
+	}
+
+}
---
+++ prac-spring/prac-spring/src/test/java/prac/pracspring/PracSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package prac.pracspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f26a832070880e2b9f09a2108f79753fe61c4888
jjwon915 <jjwon915@naver.com>
2021-08-18 22:35:33 +0900

    MVC 구조 이해

--- prac-spring/prac-spring/src/main/java/prac/pracspring/controller/SpringController.java
+++ prac-spring/prac-spring/src/main/java/prac/pracspring/controller/SpringController.java
@@ -3,6 +3,7 @@ package prac.pracspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SpringController {
@@ -12,5 +13,11 @@ public class SpringController {
         model.addAttribute("data", "spring");
         model.addAttribute("data2", "jjwon915");
         return "spring";
+    }
+
+    @GetMapping("greeting")
+    public String greet(@RequestParam(value="name", required = true) String name, Model model){
+        model.addAttribute("name", name);
+        return "greeting-user";
     }
 }

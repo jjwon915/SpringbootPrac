ea4a37469b1c451ca75132ad632830f00ed74d65
jjwon915 <jjwon915@naver.com>
2021-08-19 15:30:49 +0900

    Api 방식 - 객체로 반환하는 것. - String 형태가 아닌 객체로 반환하고, @ResponseBody 어노테이션을 붙이면 json 형태로 브라우저 상에 출력된다. - 따라서 객체의 형태로 반환했을 때 json 형태로 반환되는 방식이 Api 방식.

--- prac-spring/prac-spring/src/main/java/prac/pracspring/controller/SpringController.java
+++ prac-spring/prac-spring/src/main/java/prac/pracspring/controller/SpringController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SpringController {
@@ -19,5 +20,41 @@ public class SpringController {
     public String greet(@RequestParam(value="name", required = true) String name, Model model){
         model.addAttribute("name", name);
         return "greeting-user";
+    }
+
+    @GetMapping("string-test")
+    @ResponseBody
+    public String stringTest(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-info")
+    @ResponseBody
+    public Info helloInfo(@RequestParam("name") String name, @RequestParam("age") int age){
+        Info info = new Info();
+        info.setName(name);
+        info.setAge(age);
+        return info;
+    }
+
+    static class Info{
+        private String name;
+        private int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
     }
 }

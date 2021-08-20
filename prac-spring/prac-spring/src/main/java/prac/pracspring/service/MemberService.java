7c5a2faa2a8bc76ad1e5b82d8c709e6b46f582c1
jjwon915 <jjwon915@naver.com>
2021-08-20 13:53:12 +0900

    MemberService.java는 repository 패키지 안에서 구현한 메소드들을 사용하여 실제로 회원가입과 전체 회원 조회와 같은 메소드를 구현한 클래스 이다.

---
+++ prac-spring/prac-spring/src/main/java/prac/pracspring/service/MemberService.java
@@ -0,0 +1,43 @@
+package prac.pracspring.service;
+
+import prac.pracspring.domain.Member;
+import prac.pracspring.repository.MemberRepository;
+import prac.pracspring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입 */
+    public Long join(Member member){
+        validateDuplicateMember(member.getName());
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 회원 중복 검사 */
+    public void validateDuplicateMember(String name){
+        memberRepository.findByName(name)
+                .ifPresent(member -> {
+                   throw new IllegalStateException("중복된 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회 */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    /**
+     * Id로 회원 조회*/
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}

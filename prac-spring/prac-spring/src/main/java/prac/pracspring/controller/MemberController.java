ff0052e5dec84074a20100f2349980bbddf32a5d
jjwon915 <jjwon915@naver.com>
2021-08-21 00:36:30 +0900

    listMembers.html, MemberController.java - 회원 가입을 한 회원들의 목록이 출력되도록 구현하였다. - thymeleaf를 사용하여 for문을 도는 것처럼 MemberController에서 List<Model> 객체model을 넘겨 members로 넘어온 것을 member로 받아 id와 name 값을 꺼내서 테이블 형태로 출력한다. - 회원 가입을 한 정보들은 메모리에 저장되기 때문에 서버를 재가동하면 이전에 저장된 정보들이 사라진다.

--- prac-spring/prac-spring/src/main/java/prac/pracspring/controller/MemberController.java
+++ prac-spring/prac-spring/src/main/java/prac/pracspring/controller/MemberController.java
@@ -2,10 +2,13 @@ package prac.pracspring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import prac.pracspring.domain.Member;
 import prac.pracspring.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -30,5 +33,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> list = memberService.findMembers();
+        model.addAttribute("members", list);
+        return "members/listMembers";
     }
 }

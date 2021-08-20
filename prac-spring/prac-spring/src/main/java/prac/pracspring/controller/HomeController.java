19f4515a9f773eb849a5a4d4075dbc02b3603a82
jjwon915 <jjwon915@naver.com>
2021-08-21 00:22:15 +0900

    HomeController - 메인 페이지 설정 Controller.

--- prac-spring/prac-spring/src/main/java/prac/pracspring/controller/HomeController.java
+++ prac-spring/prac-spring/src/main/java/prac/pracspring/controller/HomeController.java
@@ -2,6 +2,9 @@ package prac.pracspring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import prac.pracspring.domain.Member;
+import prac.pracspring.service.MemberService;
 
 @Controller
 public class HomeController {
@@ -10,4 +13,5 @@ public class HomeController {
     public String home(){
         return "home";
     }
+
 }

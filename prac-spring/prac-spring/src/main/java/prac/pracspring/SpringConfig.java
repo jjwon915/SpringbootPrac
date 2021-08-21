7ebe04719d9535826f33ff18df7fa346a51b0fc5
jjwon915 <jjwon915@naver.com>
2021-08-21 19:14:31 +0900

    JPA - 반복적인 코드를 사용하지 않아도 되고, 기본적인 쿼리도 JPA가 만들어서 실행해준다. - JPA를 통해 객체 중심 설계를 할 수 있다.

--- prac-spring/prac-spring/src/main/java/prac/pracspring/SpringConfig.java
+++ prac-spring/prac-spring/src/main/java/prac/pracspring/SpringConfig.java
@@ -5,24 +5,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import prac.pracspring.repository.JdbcMemberRepository;
 import prac.pracspring.repository.JdbcMemberTemplateRepository;
+import prac.pracspring.repository.JpaMemberRepository;
 import prac.pracspring.repository.MemberRepository;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
+    /*private DataSource dataSource;
 
     @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
-    }
+    }*/
 
     @Bean
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcMemberTemplateRepository(dataSource);
+        //return new JdbcMemberTemplateRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- prac-spring/prac-spring/src/main/java/prac/pracspring/service/MemberService.java
+++ prac-spring/prac-spring/src/main/java/prac/pracspring/service/MemberService.java
@@ -2,6 +2,7 @@ package prac.pracspring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import prac.pracspring.domain.Member;
 import prac.pracspring.repository.MemberRepository;
 
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
